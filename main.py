67fdffd1188e7ebd56b503bdc4dc3e7919864dca
Alireza Mohammadi <alireza1384928@gmail.com>
2025-12-18 21:08:51 +0330

    Improve news fetching, Export HTML-based email message to a function.

--- main.py
+++ main.py
@@ -7,7 +7,22 @@ API_KEY = "7f38ced1aeda423294e57b50ee73ff9f"
 TODAY_DATE = date.today().strftime("%Y-%m-$d")
 
 
-def get_news(topic: str, from_date: ... = TODAY_DATE, n_news = 10) -> list:
+def make_html_msg(news_func):
+    def wrapper(*args, **kwargs):
+        todays_news = news_func(*args, **kwargs)
+
+        msg = ""
+        for news in todays_news:
+            msg += f"<b>{news["title"]}</b><br>" \
+                f"{news["desc"]}<br>" \
+                f"{news["url"]}<br><br><br>"
+
+        return msg
+    return wrapper
+
+
+@make_html_msg
+def fetch_news(topic: str, from_date: str = TODAY_DATE, n_news = 15) -> list:
 
     URL = f"https://newsapi.org/v2/everything?q={topic}" \
           f"&from={from_date}&sortBy=popularity&searchIn=title" \
@@ -23,8 +38,6 @@ def get_news(topic: str, from_date: ... = TODAY_DATE, n_news = 10) -> list:
         news = {
             "title": article["title"],
             "desc": article["description"],
-            "content": article["content"],
-            "source": article["source"]["name"],
             "url": article["url"]
         }
         news_list.append(news)
@@ -33,7 +46,5 @@ def get_news(topic: str, from_date: ... = TODAY_DATE, n_news = 10) -> list:
 
 
 if __name__ == "__main__":
-    todays_news = get_news("Samsung")
-    news2 = todays_news[2]
-
-    send_email(news2["title"], f"{news2["desc"]}\n\n\nNews by: {news2["source"]}\n\n{news2["url"]}")
+    msg = fetch_news("Gemini")
+    send_email("Today's News", msg)
